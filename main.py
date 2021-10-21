52ed9cf7439814b11e524d596edbac72f73e123a
Mpho J Kubeka <kubeka7712@gmail.com>
2021-10-22 00:33:50 +0200

    Updated metrics
    Added filtering (if statements) to make the readings dynamic.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from mcstatus import MinecraftServer
 
-
-cols1, cols2, cols3 = st.columns([1,6,1])
+cols1, cols2, cols3 = st.columns([1, 6, 1])
 
 with cols1:
     st.write("")
@@ -15,16 +14,15 @@ with cols2:
 
 with cols3:
     st.write("")
-    
 
-server_name_list = {'hub.manacube.net':'https://manacube.com/',
-                    'hub.lemoncloud.net':'https://lemoncloud.net',
-                    'PURPLEPRISON.NET':'https://www.purpleprison.co/',
-                    'join.miningdead.com':'https://havoc.games/',
-                    'hub.mineville.org':'https://manacube.com/',
-                    'og.universemc.us':'https://universemc.us/',
-                    'Herobrine.org':'https://herobrine.org/',
-                    'bedwars.games':'https://blockdrop.org/index.php'}
+server_name_list = {'hub.manacube.net': 'https://manacube.com/',
+                    'hub.lemoncloud.net': 'https://lemoncloud.net',
+                    'PURPLEPRISON.NET': 'https://www.purpleprison.co/',
+                    'join.miningdead.com': 'https://havoc.games/',
+                    'hub.mineville.org': 'https://manacube.com/',
+                    'og.universemc.us': 'https://universemc.us/',
+                    'Herobrine.org': 'https://herobrine.org/',
+                    'bedwars.games': 'https://blockdrop.org/index.php'}
 
 for key, value in server_name_list.items():
     server = MinecraftServer.lookup(key)
@@ -35,12 +33,27 @@ for key, value in server_name_list.items():
     st.title(f'{key} server')
 
     # Calculate percentage of online players against total joined players
-    drop = round((int(server_status.players.online) / (int(server_status.players.max)) * 100))
-
     col1, col2, col3 = st.columns(3)
 
-    col1.metric('Total players in server', server_status.players.max, delta='> 500 players')
-    col2.metric('Players currently online', server_status.players.online, delta=f'{drop}% of members')
-    col3.metric('Ping', server_status.latency, delta='Over 100ms', delta_color='inverse')
+    # Filtering total players
+    if server_status.players.max > 1000:
+        col1.metric('Total signed players', server_status.players.max, delta='> 1000 joined players')
+    else:
+        col1.metric(f'Total signed players', server_status.players.max, delta=f'< 1000 joined players',
+                    delta_color='inverse')
+
+    # Filtering active/currently online players
+    drop = round((int(server_status.players.online) / (int(server_status.players.max)) * 100))
+    if drop > 50:
+        col2.metric('Players online', f'{drop}%', delta=f'< half of members online', delta_color='inverse')
+    else:
+        col2.metric('Players online', f'{drop}%', delta=f'> half of members online')
+
+
+    # Filtering latency
+    if round(server_status.latency) > 300:
+        col3.metric('Ping', round(server_status.latency), delta='Ping over 300ms', delta_color='inverse')
+    else:
+        col3.metric('Ping', round(server_status.latency), delta='Ping under 300ms')
 
     st.markdown(f'For more info about **{key}** server, go to {value}', unsafe_allow_html=True)
