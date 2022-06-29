6be10c843d3e128470377f0af59d040aeb9df4b4
Mpho J Kubeka <kubeka7712@gmail.com>
2022-06-29 14:22:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,35 +32,38 @@ server_name_list = {'hub.manacube.net': 'https://manacube.com/',
                     'bedwars.games': 'https://blockdrop.org/index.php'}
 
 for key, value in server_name_list.items():
-    server = MinecraftServer.lookup(key)
-    server_status = server.status()
+    try:
+        server = MinecraftServer.lookup(key)
+        server_status = server.status()
 
-    st.write('----------------------')
+        st.write('----------------------')
 
-    st.title(f'{key} server')
+        st.title(f'{key} server')
 
-    # Calculate percentage of online players against total joined players
-    col1, col2, col3 = st.columns(3)
+        # Calculate percentage of online players against total joined players
+        col1, col2, col3 = st.columns(3)
 
-    # Filtering total players
-    if server_status.players.max > 1000:
-        col1.metric('Total signed players', server_status.players.max, delta='> 1000 joined players')
-    else:
-        col1.metric(f'Total signed players', server_status.players.max, delta=f'< 1000 joined players',
-                    delta_color='inverse')
+        # Filtering total players
+        if server_status.players.max > 1000:
+            col1.metric('Total signed players', server_status.players.max, delta='> 1000 joined players')
+        else:
+            col1.metric(f'Total signed players', server_status.players.max, delta=f'< 1000 joined players',
+                        delta_color='inverse')
 
-    # Filtering active/currently online players
-    drop = round((int(server_status.players.online) / (int(server_status.players.max)) * 100))
-    if drop > 50:
-        col2.metric('Players online', f'{drop}%', delta=f'< half of members online', delta_color='inverse')
-    else:
-        col2.metric('Players online', f'{drop}%', delta=f'> half of members online')
+        # Filtering active/currently online players
+        drop = round((int(server_status.players.online) / (int(server_status.players.max)) * 100))
+        if drop > 50:
+            col2.metric('Players online', f'{drop}%', delta=f'< half of members online', delta_color='inverse')
+        else:
+            col2.metric('Players online', f'{drop}%', delta=f'> half of members online')
 
 
-    # Filtering latency
-    if round(server_status.latency) > 300:
-        col3.metric('Ping', round(server_status.latency), delta='Ping over 300ms', delta_color='inverse')
-    else:
-        col3.metric('Ping', round(server_status.latency), delta='Ping under 300ms')
+        # Filtering latency
+        if round(server_status.latency) > 300:
+            col3.metric('Ping', round(server_status.latency), delta='Ping over 300ms', delta_color='inverse')
+        else:
+            col3.metric('Ping', round(server_status.latency), delta='Ping under 300ms')
 
-    st.markdown(f'For more info about **{key}** server, go to {value}', unsafe_allow_html=True)
+        st.markdown(f'For more info about **{key}** server, go to {value}', unsafe_allow_html=True)
+   except:
+        pass
